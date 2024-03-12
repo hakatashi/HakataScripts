2eb054517bb1e6345b445303fa89a8523235c5e7
Koki Takahashi <hakatasiloving@gmail.com>
2024-03-12 13:36:24 +0000

    Add pubsub test script

---
+++ pubsub-test.py
@@ -0,0 +1,23 @@
+import json
+from google.cloud import pubsub_v1
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google-application-credentials.json'
+
+project_id = "hakatabot-firebase-functions"
+topic_id = "hakatabot"
+
+publisher = pubsub_v1.PublisherClient()
+# The `topic_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/topics/{topic_id}`
+topic_path = publisher.topic_path(project_id, topic_id)
+
+for n in range(1, 10):
+    data_str = json.dumps({'message': f"Message number {n}", 'n': n})
+    # Data must be a bytestring
+    data = data_str.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data)
+    print(future.result())
+
+print(f"Published messages to {topic_path}.")
